3bc762ca84b191fc1087f6ba5063e2ec78597357
mateuszrus <mateuszrus92@gmail.com>
2021-12-05 19:26:13 +0100

    Counting Sort

---
+++ CountingSort.py
@@ -0,0 +1,24 @@
+def countSort(arr):
+    output = [0 for i in range(len(arr))]
+    count = [0 for i in range(256)]
+
+    for i in arr:
+        count[ord(i)] += 1
+
+    for i in range(256):
+        count[i] += count[i - 1]
+
+    for i in range(len(arr)):
+        output[count[ord(arr[i])] - 1] = arr[i]
+        count[ord(arr[i])] -= 1
+
+    return output
+
+
+if __name__ == '__main__':
+    arr = "MateuszRus1992Programowaniezpasja"
+    ans = countSort(arr)
+    print("Lista posortowanych znaków w tablicy % s" % ("".join(ans)))
+
+
+
