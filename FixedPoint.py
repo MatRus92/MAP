e3637f038ee1c640201adb3e702079614beba488
MatRus92 <kontakt@mateuszrus.pl>
2021-12-26 11:06:27 +0100

    Fixed Point

---
+++ FixedPoint.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def fixed_point(arr: List[int]):
+    for index, value in enumerate(arr):
+        if value == index:
+            return value
+        elif value > index:
+            continue
+    return False
+
+
+if __name__ == "__main__":
+    print(fixed_point([1, 5, 7, 8]))
+    print(fixed_point([-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13]))
+    print(fixed_point([-6, 0, 40, 3]))
+
+    
