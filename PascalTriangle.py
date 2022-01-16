044467c4c0c0134a1fc96765d2fc9336ef43377f
MatRus92 <kontakt@mateuszrus.pl>
2022-01-16 14:18:39 +0100

    Pascal Triangle

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -9,8 +9,6 @@ def get_pascal(k: int) -> List[int]:
         last = 0
         for i in range(curr - 1):
             last, temp = row[i], last
-            print(last)
-            print(temp)
             row[i] += temp
         curr += 1
     return row
